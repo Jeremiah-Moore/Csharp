31618bfaff0beca8ac2cfcf05e67516b7493d67b
Jeremiah Moore <gpsouthmoore@gmail.com>
2022-10-27 14:00:05 -0400

    Initial commit

---
+++ ConsoleAppLearning/ConsoleAppLearning/Menu.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLearning
+{
+    internal class Menu
+    {
+        GameEngine gameEngine = new();
+        internal void ShowMenu(string name, DateTime date)
+        {
+            Console.Clear();
+            Console.WriteLine($"Hello {name.ToUpper()} It's {date}. This is your math game. That's great that you're working on improving yourself");
+            Console.WriteLine("Press any key to show menu");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+
+            var isGameOn = true;
+            //above variable is boolean or bool
+
+            do //everthing inside this do brack until while will happen while the game is on
+            {
+                Console.Clear();
+                Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+V - View Previous Games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+                Console.WriteLine("-------------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.PrintGames();
+                        break;
+                    case "a":
+                        gameEngine.AdditionGame("Addition game");
+                        break;
+                    case "s":
+                        gameEngine.SubtractionGame("Subtraction game");
+                        break;
+                    case "m":
+                        gameEngine.MultiplicationGame("Multiplication game");
+                        break;
+                    case "d":
+                        gameEngine.DivisionGame("Division game");
+                        break;
+                    case "q":
+                        Console.WriteLine("GoodBye");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+
+            } while (isGameOn);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleAppLearning/ConsoleAppLearning/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleAppLearning.Models;
+
+internal class Game
+{
+    //private int _score;
+    //public int Score 
+    //{ 
+    //    get { return _score; } 
+    //    set { _score = value; }
+    //}
+    internal DateTime Date { get; set; }
+
+    internal int Score { get; set; }
+
+    internal GameType Type { get; set; }
+
+}
+
+internal enum GameType
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+}
