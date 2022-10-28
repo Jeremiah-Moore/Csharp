0995e89a1266f5d52036f4b55235c762e85db3a7
Jeremiah Moore <gpsouthmoore@gmail.com>
2022-10-28 15:23:54 -0400

    still in the process of making the app with a slight design

--- MathGame/MathGame/MainPage.xaml.cs
+++ MathGame/MathGame/MainPage.xaml.cs
@@ -2,23 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnGameChosen(object sender, EventArgs e)
 	{
-		count++;
+		Button button = (Button)sender;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Navigation.PushAsync(new Gamepage(button.Text));
 	}
+
+    private void OnViewPreviousGamesChosen(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new PreviousGames());
+    }
 }
 
