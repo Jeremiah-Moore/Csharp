7e0e858941dd1fad70b5f9811cc50a96c19f970a
Jeremiah Moore <gpsouthmoore@gmail.com>
2022-10-29 22:33:55 -0400

    Saving before I delete
    going to try to split the app into classes

--- Calculator_Console/Calculator_Console/Program.cs
+++ Calculator_Console/Calculator_Console/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Declare variables and then initialize to zero.
-int num1 = 0; int num2 = 0; //(num1 and num2 are the variables) 0 is there value
-// int = initialize
+double num1 = 0; double num2 = 0; //(num1 and num2 are the variables) 0 is there value
+// double = initialize
 
 //Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
@@ -8,12 +8,12 @@ Console.WriteLine("-------------------------\n");
 
 //Ask the user to type the first(num1) number
 Console.WriteLine("Type a number, and then press enter");
-num1 = Convert.ToInt32(Console.ReadLine()); // console write line is what ever question or statement I ask or tell the user
+num1 = Convert.ToDouble(Console.ReadLine()); // console write line is what ever question or statement I ask or tell the user
 //Console.ReadLine is the user's response or answer
 
 //Ask the user to type the second(num2) number
 Console.WriteLine("Type a number, and then press enter");
-num2 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToDouble(Console.ReadLine());
 
 //Ask the user to choose an option
 Console.WriteLine("Choose an option from the following list:");
@@ -36,9 +36,19 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} =" + (num1 * num2));
         break;
     case "d":
+
+        //Ask the user to enter a number that's not zero until the 0 is replaced
+        while(num2 == 0)
+        {
+            Console.WriteLine("Enter a non zero divisor:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} =" + (num1 / num2));
-        break;
+        break ;
 }
 //Wait for the user  to respond before closing.
 Console.WriteLine("Press any key to close the Calculator console app...");
 Console.ReadLine();
+
+
+
